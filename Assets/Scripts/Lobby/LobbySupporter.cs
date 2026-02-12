7044bb83df84ed831542d738381221eb2a8b5b19
etote306 <etote306@gmail.com>
2026-02-12 18:50:24 +0900

    ver: 1.015 1. Boss 스테이지에서 경고 UI의 속도를 조금 늦춤 2. UI 버튼들 배치 3. UI 연결중

--- Assets/Scripts/Lobby/LobbySupporter.cs
+++ Assets/Scripts/Lobby/LobbySupporter.cs
@@ -9,6 +9,7 @@ public class LobbySupporter : MonoBehaviour
     public Sprite LockedSprite; // 획득 전 스프라이트
     public Sprite UnlockedSprite; // 획득 후 스프라이트
     public GameObject InfoPopup; // 서포터 정보 팝업창
+    public Sprite HighlightVisual; // 스쿼드 편성 여부를 알려주는 하이라이트 테두리
 
     private Image SupporterImage; // 서포터 스프라이트 변경을 위한 이미지 컴포넌트
     private Button SupporterButton; // 서포터를 클릭할 수 있게 하기 위한 버튼 컴포넌트
@@ -44,11 +45,37 @@ public class LobbySupporter : MonoBehaviour
     /// <summary>
     /// Button 컴포넌트에 연결 위한 메서드
     /// 서포터 클릭 시 호출
+    /// 스쿼드 편성 모드에 따라 스쿼드 편성 or 팝업 띄우기
     /// </summary>
     public void OnClickSupporter()
     {
         if (!isUnlocked) return; // 미획득 시 리턴
 
+        if (SquadManager.Instance.isSquadMode)
+        {
+            bool selet = SquadManager.Instance.ToggleSelectSupporter(SupporterID);
+            if (selet) UpdateHighlight();
+        }
+        else
+        {
+            OpenInfoPopup();
+        }
+    }
+
+    /// <summary>
+    /// 하이라이트를 띄울 메서드
+    /// </summary>
+    public void UpdateHighlight()
+    {
+        bool isSelected = SquadManager.Instance.SelectedSquad.Contains(SupporterID);
+        SupporterImage.sprite = HighlightVisual;
+    }
+
+    /// <summary>
+    /// 팝업을 띄우는 메서드
+    /// </summary>
+    void OpenInfoPopup()
+    {
         InfoPopup.SetActive(true); // 팝업 띄우기
     }
 }
--- Assets/Scripts/Lobby/SquadManager.cs
+++ Assets/Scripts/Lobby/SquadManager.cs
@@ -1,28 +1,57 @@
 using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using Unity.VisualScripting;
 
 public class SquadManager : MonoBehaviour
 {
     // 스테이지에 데려갈 서포터를 골라 스쿼드 만들기
     // 최대 2명
+    public static SquadManager Instance;
+
+    // 서포터 기본 정보 세팅
+    [Header("Status")]
     public List<int> SelectedSquad; // 선택된 ID 리스트
+    public bool isSquadMode; // 스쿼드 편성 모드인지
+    public Sprite SelectedVisual; // 선택 후 비주얼
+
+    // UI 세팅
+    [Header("UI Buttons")]
+    public Button SquadModeButton; // 스쿼드 편성 버튼
     public Button SaveButton; // 스쿼드 저장 버튼
+
+    private Sprite originSprite; // 선택 전 비주얼
+
+    void Awake() => Instance = this;
 
     private void Start()
     {
         SelectedSquad = new List<int>();
-        SaveButton.interactable = false; // 비활성화 상태로 초기화
+        SaveButton.gameObject.SetActive(false); // 비활성화 상태로 초기화
+        isSquadMode = false;
         LoadSquad();
+
+        originSprite = GetComponent<Sprite>();
+    }
+
+    public void ToggleSquadMode()
+    {
+        isSquadMode = !isSquadMode;
+
+        SaveButton.gameObject.SetActive(isSquadMode);
+
+        Debug.Log(isSquadMode ? "Squad Mode True" : "Squad Mode False");
     }
 
     /// <summary>
     /// 스쿼드를 만들기 위해 서포터를 토글로 선택하는 메서드 -> 버튼에 할당
     /// 최대 2명
     /// </summary>
-    /// <param name="id"></param>
-    public void ToggleSelectSupporter(int id)
+    /// <param name = "id" ></ param >
+    public bool ToggleSelectSupporter(int id)
     {
+        if (PlayerPrefs.GetInt("Supporter_" + id, 0) != 1) return false;
+
         if (SelectedSquad.Contains(id)) // 이미 선택된 경우
         {
             SelectedSquad.Remove(id); // 제거
@@ -36,13 +65,14 @@ public class SquadManager : MonoBehaviour
             else // 2명을 초과한 경우
             {
                 Debug.Log("Maximum Supporters in Squad is 2");
-                return;
             }
         }
 
         SaveButton.interactable = SelectedSquad.Count > 0; // 하나라도 선택되면 활성
 
-        // 추가적인 하이라이트 UI 업데이트 로직 필요
+        // 추가적인 하이라이트 UI 업데이트 로직 필요 -> 하이라이트 여부까지 Prefab Save 해야 할 듯
+
+        return true;
     }
 
     /// <summary>
@@ -54,6 +84,7 @@ public class SquadManager : MonoBehaviour
         PlayerPrefs.SetInt("Squad_Slot1", SelectedSquad.Count > 1 ? SelectedSquad[1] : -1); // 하나보다 많이 있으면 Squad_Slot 1번 Element 삽입, 없으면 -1
         PlayerPrefs.Save(); // 저장
 
+        isSquadMode = false;
         SaveButton.interactable = false; // 저장 후 비활성화
         Debug.Log("Squad Save!");
     }
